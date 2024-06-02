bc82bfee45b5bbde06be268cde5254c16fc4d933
Usman Khan <usmankhan0265@gmail.com>
2024-06-02 18:22:45 +0530

    added sources

---
+++ script.js
@@ -0,0 +1,18 @@
+const navDialog = document.getElementById('nav-dialog');
+function handleMenu() {
+    navDialog.classList.toggle('hidden');
+}
+
+
+const dtElements = document.querySelectorAll('dt');
+dtElements.forEach(element => {
+    element.addEventListener('click', () => {
+        const ddId = element.getAttribute('aria-controls');
+        const ddElement= document.getElementById(ddId);
+        const ddArrowIcon = element.querySelectorAll('i')[0];
+
+        ddElement.classList.toggle('hidden');
+        ddArrowIcon.classList.toggle('-rotate-180');
+
+    })
+})
